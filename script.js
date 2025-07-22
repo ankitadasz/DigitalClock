e2330c2e85f7389e8d6a9642d61cc2f61f8af340
Ankita Das <daszankita23@gmail.com>
2025-07-22 11:21:00 +0530

    Am Pm Added

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ let hour = document.getElementById("hrs");
 let minute = document.getElementById("mins");
 let sec = document.getElementById("secs");
 let indicator = document.getElementById("ind");
-const date = new Date();
-let hrs = date.getHours();
-let min = date.getMinutes();
-let secs = date.getSeconds();
-hour.innerHTML = hrs;
-minute.innerHTML = min;
-sec.innerHTML = secs;
+
+function countDown() {
+  const date = new Date();
+  let hrs = date.getHours();
+  let min = date.getMinutes();
+  let secs = date.getSeconds();
+
+ indicator.innerHTML =  hrs >= 12 ? "PM" :"AM";
+  if (hrs > 12) {
+    hrs = hrs - 12;
+  }
+  hour.innerHTML = (hrs < 10 ? "0" : "") + hrs;
+  minute.innerHTML = (min < 10 ? "0" : "") + min;
+  sec.innerHTML = (secs < 10 ? "0" : "") + secs;
+}
+
+setInterval(countDown, 1000);
